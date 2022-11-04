e6b1c9ad0517a4715ad719f4215691ec3415a346
MansuttiD <daniel.mansutti@gmail.com>
2022-11-04 18:37:38 -0400

    first commit

---
+++ src/components/shared/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './styles/footer.css'
+
+const Footer = () => {
+
+  return (
+    <footer className='footer__container'>
+        <div className='footer__icon'>
+        <i className="fa-brands fa-instagram"></i>
+        </div>
+        <div className='footer__icon'>
+        <i className="fa-brands fa-linkedin"></i>
+        </div>
+        <div className='footer__icon'>
+        <i className="fa-brands fa-twitter"></i>
+        </div>
+
+    </footer>
+
+  )
+}
+
+export default Footer
